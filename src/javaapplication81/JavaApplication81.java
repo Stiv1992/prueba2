0d9f61c4aadb52add648888cb38cd673e4aea37e
stiv1992 <darwistiven6@gmail.com>
2015-05-21 19:40:25 -0500

    agregando mensaje

--- src/javaapplication81/JavaApplication81.java
+++ src/javaapplication81/JavaApplication81.java
@@ -18,6 +18,7 @@ public class JavaApplication81 {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("mensaje");
+        System.out.println("connectando a la nube");
     }
     
 }
